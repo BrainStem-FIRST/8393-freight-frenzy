1ddd168f35dcbb771fdc8459245f3faa67e5978e
odpgit <owen_gr@premhome.net>
2021-12-29 21:14:40 -0500

    auto attachment updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BrainSTEMAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BrainSTEMAutonomous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -7,6 +8,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.robot.BrainSTEMRobot;
+import org.firstinspires.ftc.teamcode.robot.Collector;
 import org.firstinspires.ftc.teamcode.robot.DepositorLift;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -19,6 +21,7 @@ public class BrainSTEMAutonomous extends LinearOpMode {
     private double depositTheta;
     private double depositThetaWarehouse = Math.toRadians(160);
     private double depositThetaCarousel = Math.toRadians(200);
+    private double resetTheta = Math.toRadians(180);
     protected AllianceColor color = AllianceColor.BLUE;
     protected StartLocation startLocation = StartLocation.WAREHOUSE;
     private BarcodePattern pattern = BarcodePattern.LEVELTHREE;
@@ -29,32 +32,48 @@ public class BrainSTEMAutonomous extends LinearOpMode {
 
         depositTheta = startLocation == StartLocation.WAREHOUSE ? depositThetaWarehouse : depositThetaCarousel;
 
-        robot.depositorLift.setDepositLow(true);
+        robot.depositorLift.setHeight(BarcodePattern.LEVELONE);
 //        VuforiaLocalizer vuforia = initVuforia();
 //        TFObjectDetector tfod = initTfod(vuforia);
 
-        TrajectorySequence startTrajectory = robot.drive.trajectorySequenceBuilder(coordinates.startPos())
-                .setReversed(true)
+        Trajectory seTrajectory = robot.drive.trajectoryBuilder(coordinates.startPos(), true)
                 .lineTo(coordinates.shippingElementCollect().vec())
-                .addDisplacementMarker(() -> {robot.depositorLift.clampSE();})
-                .waitSeconds(1)
-                .UNSTABLE_addTemporalMarkerOffset(1, () -> {robot.turret.autoSpinTurret(depositThetaWarehouse);})
+                .build();
+
+        Trajectory preloadTrajectory = robot.drive.trajectoryBuilder(coordinates.shippingElementCollect(), true)
                 .splineToLinearHeading(coordinates.deposit(), coordinates.depositTangent())
                 .build();
 
         TrajectorySequence warehouseSequence = robot.drive.trajectorySequenceBuilder(coordinates.shippingElementCollect())
                 .setReversed(false)
-                .UNSTABLE_addTemporalMarkerOffset(0.25, () -> {
-                    /*Turn on collector, reset turret, lift down, etc.*/
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.setGoal(DepositorLift.Goal.RETRACT);
                 })
                 .splineToSplineHeading(coordinates.cycleWaypoint1(), coordinates.cycleWaypoint1Tangent())
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.turret.autoSpinTurret(resetTheta);
+                })
                 .splineToSplineHeading(coordinates.cycleWaypoint2(), coordinates.cycleForwardTangent())
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.autoLiftDown();
+                    robot.depositorLift.setHeight(BarcodePattern.LEVELTHREE);
+                    robot.collector.setGoal(Collector.Goal.DEPLOY);
+                })
                 .splineTo(coordinates.cycleCollect().vec(), coordinates.cycleForwardTangent())
                 .waitSeconds(2)
                 .setReversed(true)
-                .addDisplacementMarker(0.25, 0, () -> {/*Turn off collector, turn turret, lift up, etc.*/})
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
+                    robot.collector.setGoal(Collector.Goal.RETRACT);
+                })
                 .splineTo(coordinates.cycleWaypoint2().vec(), coordinates.cycleReverseTangent())
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.setGoal(DepositorLift.Goal.DEPLOY);
+                })
                 .splineToSplineHeading(coordinates.cycleWaypoint1(), coordinates.depositTangent())
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.autoLiftUp();
+                    robot.turret.autoSpinTurret(depositTheta);
+                })
                 .splineToSplineHeading(coordinates.deposit(), coordinates.depositTangent())
                 .addDisplacementMarker(() -> {robot.depositorLift.open();})
                 .waitSeconds(1.5)
@@ -62,18 +81,36 @@ public class BrainSTEMAutonomous extends LinearOpMode {
 
         TrajectorySequence carouselSequence = robot.drive.trajectorySequenceBuilder(coordinates.shippingElementCollect())
                 .setReversed(false)
-                .addDisplacementMarker(0.4, 0, () -> {/*Turn on carousel wheel, reset turret, lift down, etc.*/})
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.setGoal(DepositorLift.Goal.RETRACT);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(0.7, () -> {
+                    robot.turret.autoSpinTurret(resetTheta);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(1.5, () -> {
+                    robot.depositorLift.autoLiftDown();
+                })
                 .splineToSplineHeading(coordinates.carouselDelivery(), coordinates.carouselDeliveryTangent())
-                .addDisplacementMarker(() -> {/*Turn on carousel wheels*/})
-                .waitSeconds(5)
-                .addDisplacementMarker(() -> {/*Turn off carousel wheel*/})
+                .addDisplacementMarker(() -> {robot.carouselSpin.autonomousSpinCarousel(color);})
+                .forward(1)
+                .waitSeconds(4)
+                .back(1)
+                .addDisplacementMarker(() -> {robot.carouselSpin.stopCarousel();})
                 .setReversed(true)
                 .splineToSplineHeading(coordinates.carouselWait(), coordinates.parkTangent())
                 .build();
 
         TrajectorySequence parkSequence = robot.drive.trajectorySequenceBuilder(coordinates.parkStart(), coordinates.parkTangent())
-                .addDisplacementMarker(0.5, 0, () -> {/*Reset turret,  etc.*/})
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.setGoal(DepositorLift.Goal.RETRACT);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
+                    robot.turret.autoSpinTurret(resetTheta);
+                })
                 .splineToSplineHeading(coordinates.parkWaypoint(), coordinates.parkTangent())
+                .UNSTABLE_addTemporalMarkerOffset(0.1, () -> {
+                    robot.depositorLift.autoLiftDown();
+                })
                 .splineToSplineHeading(coordinates.parkEnd(), coordinates.parkTangent())
                 .build();
 
@@ -110,14 +147,26 @@ public class BrainSTEMAutonomous extends LinearOpMode {
         }
 
         robot.drive.setPoseEstimate(coordinates.startPos());
-        robot.drive.followTrajectorySequenceAsync(startTrajectory);
+
+        robot.drive.followTrajectoryAsync(seTrajectory);
         robot.depositorLift.setGoal(DepositorLift.Goal.DEPLOY);
         sleep(500);
         robot.turret.autoSpinTurret(shippingElementTheta);
         robot.drive.waitForIdle();
+
+        robot.depositorLift.clampSE();
+        sleep(1000);
+
+        robot.drive.followTrajectoryAsync(preloadTrajectory);
+        //needs to be here bc shipping element has to be at L1
+        robot.depositorLift.setHeight(pattern);
         if (pattern != BarcodePattern.LEVELONE) {
-//            robot.depositorLift.
+            robot.depositorLift.autoLiftUp();
         }
+        robot.turret.autoSpinTurret(depositThetaWarehouse);
+        robot.drive.waitForIdle();
+        robot.depositorLift.open();
+
         if (startLocation == StartLocation.WAREHOUSE) {
             for (int i = 0; i < CYCLE_TIMES; i++) {
                 robot.drive.followTrajectorySequence(warehouseSequence);
