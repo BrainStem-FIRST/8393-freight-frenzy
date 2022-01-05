60dd926a189ad4d6e033139f948ae98dca9a2db3
odpgit <owen_gr@premhome.net>
2022-01-04 22:30:48 -0500

    initial real-world auto testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriveTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.robot.DepositorLift;
+import org.firstinspires.ftc.teamcode.robot.Turret;
+
+@TeleOp
+public class DriveTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addData("FL ticks", drive.getWheelPositions().get(0));
+            telemetry.addData("BL ticks", drive.getWheelPositions().get(1));
+            telemetry.addData("BR ticks", drive.getWheelPositions().get(2));
+            telemetry.addData("FR ticks", drive.getWheelPositions().get(3));
+            telemetry.update();
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SubsystemEncoderTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.autonomous.AllianceColor;
+import org.firstinspires.ftc.teamcode.autonomous.BarcodePattern;
+import org.firstinspires.ftc.teamcode.robot.CarouselSpin;
+import org.firstinspires.ftc.teamcode.robot.DepositorLift;
+import org.firstinspires.ftc.teamcode.robot.Turret;
+import org.firstinspires.ftc.teamcode.teleop.StickyButton;
+
+@TeleOp
+public class SubsystemEncoderTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Turret turret = new Turret(hardwareMap, new DepositorLift(hardwareMap, telemetry), telemetry);
+        waitForStart();
+        while(opModeIsActive()) {
+            turret.resetTurret();
+        }
+    }
+
+}
